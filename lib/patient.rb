f5f5b79e3761b1cab16db3cf50f153fc787465d1
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-17 19:44:03 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,9 +3,17 @@ class Patient
   def all
     @@all
   end
-  attr_accessor :name 
+  attr_accessor :name
   def initialize(name)
     @name = name
     @@all << self
+  end
+  def new_apppointment(doctor, date, i=self)
+    app = Appointment.new(i, date, doctor)
+  end
+  def appointments
+    Appointment.all.select do |app|
+      app.patient.name == @name
+    end
   end
 end
